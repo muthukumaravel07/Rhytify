6eb1b00d5ffc8eff3aef5b74127351f132386837
Bhanu Prakash Bakkolla <bhanu.b@corp.ads.valuelabs.net>
2019-02-15 11:29:59 +0530

    homecomponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { LoginService } from '../loginService/login.service';
+export interface Home {
+  key: string;
+  Value: number;
+}
+export interface Config{
+  Key: string;
+  Description: string;
+  OptionType: string;
+  Options: object;
+}
+@Component( {
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  home: Home[];
+  config: Config[];
+    constructor(private data: LoginService) { }
+    ngOnInit() {
+      this.data.getHome().subscribe(
+        data => {
+          this.home = data['ProjectSummaryList'];
+          console.log(this.home);
+        }
+      );
+      this.data.getConfig().subscribe(
+        data => {
+          this.config = data['Configurations'];
+          console.log(this.config);
+        }
+      )
+    }
 
 }
